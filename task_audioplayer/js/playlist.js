691e819708f9c95bf2534ea3165559d045674de3
cycbote <cycbot@qq.com>
2017-04-18 19:34:33 +0800

    tree

---
+++ task_audioplayer/js/playlist.js
@@ -0,0 +1,22 @@
+/**
+ * Created by cycbot on 2017/4/18.
+ */
+var playList = {
+    "result": {
+        "track": [
+            {
+                "name": "歌曲名",
+                "artists": [
+                    {
+                        "name": "演唱者"
+                    }
+                ],
+                "album": {
+                    "name": "专辑名",
+                    "picUrl": "专辑图",
+                    
+                }
+            }
+        ]
+    }
+}
---
+++ task_audioplayer/js/script.js
@@ -0,0 +1,3 @@
+/**
+ * Created by cycbot on 2017/4/18.
+ */
